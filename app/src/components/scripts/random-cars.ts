7a3fe746b650ffd5745aabec5ddf356e7f699da5
OstapchelaEst <Ostapchela@gmail.com>
2022-08-07 14:45:27 +0200

    feat:implemented all the functionality of the task

--- app/src/components/scripts/random-cars.ts
+++ app/src/components/scripts/random-cars.ts
@@ -1,13 +1,13 @@
 import { CARS_NAMES } from './data/cars-names';
 import { NEW_CARS } from './cars';
+import { RENDER_PAGE } from './render-page';
 
 export class RandomCars {
     getRandomInt(max: number) {
         return Math.floor(Math.random() * max);
     }
-
     getRandomCars() {
-        for (let i = 0; i < 20; i++) {
+        for (let i = 0; i < 100; i++) {
             NEW_CARS.addCar({
                 name: `${CARS_NAMES[0][this.getRandomInt(18)]} ${CARS_NAMES[1][this.getRandomInt(11)]}`,
                 color: `${this.getRandomColor()}`,
@@ -22,12 +22,12 @@ export class RandomCars {
         }
         return color;
     }
-
     addListenerCreateRandomCasrds() {
         (<HTMLButtonElement>document.querySelector('.buttons-interface__generate-cards')).addEventListener(
             'click',
-            () => {
-                this.getRandomCars();
+            async () => {
+                await this.getRandomCars();
+                await RENDER_PAGE.getTrack();
             }
         );
     }
